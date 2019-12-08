7c2559e1af92f5d896ec52084cdbeee1e34bdfdb
Andrea Thomas <ANDREA.THOMAS19460@GMAIL.COM>
2019-12-08 11:54:30 -0800

    added js file, linked to html

---
+++ script.js
@@ -0,0 +1,28 @@
+function generatePassword() {
+    var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
+
+function generate( length = 12 ){
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var numbers = '0123456789';
+var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
+
+var all = uppercase + lowercase + numbers + symbols;
+
+var password = '';
+
+for (var index = 0; index < length; index++) {
+var character = Math.floor(Math.random() * all.length);
+password += all.substring(character, character + 1);
+}
+
+return password;
+}
