2209f115132195de42549ff90beb402175720d29
Andrea Thomas <ANDREA.THOMAS19460@GMAIL.COM>
2019-12-09 20:43:20 -0800

    Functions work, sans Clipboard function

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
-
-var howManyChar = prompt('Hello! How many characters should your password be?');
-alert('The number of characters in your password will be ' + howManyChar)
-
-var includeSpecial = confirm('Include special characters?');
-var includeNums = confirm('Include numbers?');
-var includeLower = confirm('Include lowercase letters?');
-var includeUpper = confrim('Include uppercase letters?');
-
-function generate( length = 8 ){
 var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var lowercase = 'abcdefghijklmnopqrstuvwxyz';
 var numbers = '0123456789';
 var specialChar = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
 
-var charSet = uppercase + lowercase + numbers + specialChar;
+var howManyChar;
+var includeSpecial;
+var includeLower;
+var includeUpper;
 
-var password = '';
-
-for (var i = 0; n = charSet.length; i < length; i++) {
-var charSet = Math.floor(Math.random() * all.length);
-password += charSet.charAt(Math.floor(Math.random() * n));
+function getPrefs(){
+    howManyChar = prompt('Hello! How many characters should your password be? Choose a length from 8-128.');
+    if (howManyChar < 8) {
+        alert(`${howManyChar} is too small. Increasing to 8.`);
+        howManyChar = 8;
+    }
+    else if (howManyChar > 128) {
+        alert(`${howManyChar} is too large. Decreasing to 128.`);
+        howManyChar = 128;
+    }
+    else {
+        alert(`The number of characters in your password will be ${howManyChar}`);
+    }
+        
+    includeSpecial = confirm('Include special characters?');
+    includeNums = confirm('Include numbers?');
+    includeLower = confirm('Include lowercase letters?');
+    includeUpper = confirm('Include uppercase letters?');
+} 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-return password;
+function generate() {
+    var charSet = ''; 
+    if (includeUpper) {
+        charSet += uppercase;
+    }
+    if (includeLower) {
+        charSet += lowercase;
+    }
+    if (includeNums) {
+        charSet += numbers;
+    } 
+    if (includeSpecial) {
+        charSet += specialChar;
+    }
+    var password = '';
+    for (var i = 0; i < howManyChar; i++) {
+        password += charSet.charAt(getRandomInt(0, charSet.length));
+    // var charSet = Math.floor(Math.random() * all.length);
+    // password += shuffle.charAt(Math.floor(Math.random() * shuffle.length));
+    }
+    document.getElementById("passwordBox").value = password;
 }
 
-
-function myClipboard(){
+function myClipboard() {
     var myClipboard = document.getElementById("passwordBox");
     myClipboard.select();
     myClipboard.setSelectionRange(0, 99999); 
